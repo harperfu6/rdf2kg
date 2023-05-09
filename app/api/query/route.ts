6e61fc208079126515fc95095a4b8337064ac411
harperfu6 <beharp8a8@gmail.com>
2023-05-09 20:40:10 +0900

    fix to use dbpedia (necessary to use 'GET' method!)

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -2,15 +2,21 @@ import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
 import arrayifyStream from "arrayify-stream";
 
 const queryReq = async () => {
-  const fether = new SparqlEndpointFetcher();
-  const endpoint = "https://dbpedia.org/sparql";
+	const config = {
+		method: "GET",
+	}
+  const fether = new SparqlEndpointFetcher(config);
+  const endpoint = "http://ja.dbpedia.org/sparql";
   const query = `
-		PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-		SELECT ?p ?c WHERE {
-			?p a dbpedia-owl:Artist.
-    	?p dbpedia-owl:birthPlace ?c.
-    	?c <http://xmlns.com/foaf/0.1/name> "York"@en.
-		}
+PREFIX prop-ja: <http://ja.dbpedia.org/property/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT DISTINCT ?label ?depiction
+WHERE {
+  ?s prop-ja:genre <http://ja.dbpedia.org/resource/ロック_(音楽)> ;
+    rdfs:label ?label .
+  OPTIONAL { ?s foaf:depiction ?depiction .  }
+} LIMIT 100
 		`;
   return await arrayifyStream(await fether.fetchBindings(endpoint, query));
 };
